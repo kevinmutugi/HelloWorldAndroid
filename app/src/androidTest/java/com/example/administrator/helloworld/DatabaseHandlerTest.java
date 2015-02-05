351966bbbe3ed73fcb3d621f8ba3935c70039cf3
kevin mutugi <kevinmk200@gmail.com>
2015-02-05 16:46:41 +0300

    Initial commit

---
+++ app/src/androidTest/java/com/example/administrator/helloworld/DatabaseHandlerTest.java
@@ -0,0 +1,7 @@
+package com.example.administrator.helloworld;
+
+import junit.framework.TestCase;
+
+public class DatabaseHandlerTest extends TestCase {
+
+}
---
+++ app/src/main/java/com/example/administrator/helloworld/SongsActivity.java
@@ -0,0 +1,10 @@
+package com.example.administrator.helloworld;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SongsActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.songsactivity);
+    }
+}
